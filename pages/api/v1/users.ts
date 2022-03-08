4f6a99d4237a2911c0df451bb8b6aed0edee1001
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-03-08 14:53:16 +0100

    Start iterating on user signup feature

--- pages/api/v1/users.ts
+++ pages/api/v1/users.ts
@@ -6,9 +6,31 @@ type Data = {
   message: string;
 };
 
+const validateReq = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  // This acts as a middleware to validate the request
+  // if a response is returned, it will be sent to the client
+  // if no response is returned, the request will continue to the parent handler
+  res.status(200).json({ status: "success", message: "Hijacked the response" });
+};
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  if (req.method !== "POST") {
+    res
+      .setHeader("Allow", "POST")
+      .status(405)
+      .json({ status: "error", message: "Method not allowed" });
+
+    return;
+  }
+
+  validateReq(req, res);
+
+  const { body } = req;
+  const { email, password } = body;
+
+  console.log(body);
   res.status(200).json({ status: "success", message: "GET user" });
 }
