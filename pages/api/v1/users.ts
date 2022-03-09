f3016d0f6553cb9b47d51558c947ed3abbbccdb9
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-03-09 21:42:57 +0100

    Partial implementation of middleware chain utility function (#3)

--- pages/api/v1/users.ts
+++ pages/api/v1/users.ts
@@ -1,16 +1,69 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+
+import validator from "validator";
 
 type Data = {
   status: string;
   message: string;
+  data?: unknown;
 };
 
+type Middleware = (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+  next?: Middleware
+) => void;
+
+// [validateReq, handleReq]
+
+function middlewareChain(...middlewareFns: Middleware[]) {
+  return (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const middleware = middlewareFns.slice();
+    const next = () => {
+      const fn = middleware.shift();
+      if (fn) {
+        fn(req, res, next);
+      }
+    };
+    next();
+  };
+}
+
 const validateReq = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // This acts as a middleware to validate the request
-  // if a response is returned, it will be sent to the client
-  // if no response is returned, the request will continue to the parent handler
-  res.status(200).json({ status: "success", message: "Hijacked the response" });
+  const { body } = req;
+  const { email, password } = body;
+  const errors = [];
+  console.log("Gets here!", { email, password });
+
+  if (!email || !password) {
+    return res.status(400).json({
+      status: "error",
+      message:
+        "Please provide both email and password in the body of the request",
+    });
+  }
+
+  if (!validator.isEmail(email)) {
+    errors.push({
+      status: "fail",
+      message: "Email is not valid",
+    });
+  }
+
+  if (!validator.isLength(password, { min: 8 })) {
+    errors.push({
+      status: "fail",
+      message: "Password must be at least 8 characters",
+    });
+  }
+
+  if (errors.length) {
+    return res.status(422).json({
+      status: "fail",
+      message: "Validation failed",
+      data: errors,
+    });
+  }
 };
 
 export default function handler(
@@ -26,11 +79,13 @@ export default function handler(
     return;
   }
 
-  validateReq(req, res);
+  // middlewareChain()(req, res);
 
-  const { body } = req;
-  const { email, password } = body;
+  // validateReq(req, res);
 
-  console.log(body);
-  res.status(200).json({ status: "success", message: "GET user" });
+  // const { body } = req;
+  // const { email, password } = body;
+
+  // console.log(body, "Gets here too");
+  // res.status(200).json({ status: "success", message: "GET user" });
 }
